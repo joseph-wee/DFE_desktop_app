425043659fba66c8f8c0e0708f4fc6918c1573dc
joseph-wee <starlightkor@gmail.com>
2023-06-02 07:46:06 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import { GlobalNavBar } from './components';
+import { HomePage, ListPage, WirtePage } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalNavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/write" element={<WirtePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
